7b923cc9bee4ac58d63089535806ee90f0a78c33
Hans Kokx <hans.d.kokx@gmail.com>
2023-10-21 19:09:04 -0400

    WIP: Fixing panel width calculations
    Signed-off-by: Hans Kokx <hans.d.kokx@gmail.com>

--- lib/features/panel.dart
+++ lib/features/panel.dart
@@ -19,69 +19,91 @@ class ChubbyPanel {
     this.alignment = TextAlignment.left,
   });
 
-  void render(Console console) {
-    final int titleLength = title != null ? title!.text.length + 2 : 0;
-    final int panelWidth = _calculatePanelWidth(
-      text,
-      titleLength,
-      width,
-      console.windowWidth,
-    );
+  void _renderTitle(
+    ChubbyPanelTitle title,
+    LineStyle lineStyle,
+    CornerStyle cornerStyle,
+    int width,
+    Console console,
+  ) {
+    final int titleLength = title.text.length + 2;
 
-    print("Panel width: $panelWidth, Console width: ${stdout.terminalColumns}");
+    final String ptl = title.style.tl;
+    final String ptr = title.style.tr;
+    final String pbl = title.style.bl;
+    final String pbr = title.style.br;
 
-    final int widthCompensation = width != null ? 5 : 1;
-
-    final CornerStyle cornerStyle = title?.style ?? style;
-
-    final LineStyle line = title?.style == CornerStyle.doubled
+    final LineStyle lineStyle = title.style == CornerStyle.doubled
         ? LineStyle.doubled
         : LineStyle.single;
 
-    final String ptl = cornerStyle.tl;
-    final String ptr = cornerStyle.tr;
-    final String pbl = cornerStyle.bl;
-    final String pbr = cornerStyle.br;
-    final String px = line.x;
+    final String px = lineStyle.x;
 
-    String vl = line.vl;
-    String vr = line.vr;
+    String vl = lineStyle.vl;
+    String vr = lineStyle.vr;
 
-    String x = line.x;
-    String y = line.y;
+    String x = lineStyle.x;
+    String y = lineStyle.y;
 
-    if (title?.style != CornerStyle.doubled && style == CornerStyle.doubled) {
-      vl = line.ovl;
-      vr = line.ovr;
+    if (title.style != CornerStyle.doubled &&
+        cornerStyle == CornerStyle.doubled) {
+      vl = lineStyle.ovl;
+      vr = lineStyle.ovr;
 
       x = LineStyle.doubled.x;
       y = LineStyle.doubled.y;
     }
 
-    if (title?.style == CornerStyle.doubled && style != CornerStyle.doubled) {
-      vl = line.ovl;
-      vr = line.ovr;
+    if (title.style == CornerStyle.doubled &&
+        cornerStyle != CornerStyle.doubled) {
+      vl = lineStyle.ovl;
+      vr = lineStyle.ovr;
 
       x = LineStyle.single.x;
       y = LineStyle.single.y;
     }
 
-    if (title != null) {
-      console.writeLine(
-        '  $ptl${px * (title!.text.length + 2)}$ptr',
-      );
+    final int widthMinusTitlePanel = width - title.text.length - 7;
 
-      console.writeLine(
-        '${style.tl}$x$vl ${title!.text} $vr${x * (panelWidth - widthCompensation)}${style.tr}',
-      );
-      console.writeLine(
-        '$y $pbl${px * titleLength}$pbr${" " * (panelWidth - widthCompensation)}$y',
-      );
+    console.writeLine(
+      '  $ptl${px * (title.text.length + 2)}$ptr',
+    );
+
+    console.writeLine(
+      '${style.tl}$x$vl ${title.text} $vr${x * widthMinusTitlePanel}${style.tr}',
+    );
+    console.writeLine(
+      '$y $pbl${px * titleLength}$pbr${" " * widthMinusTitlePanel}$y',
+    );
+  }
+
+  void render(Console console) {
+    final int panelWidth = _calculatePanelWidth(width, text, title?.text);
+
+    final LineStyle lineStyle = title?.style == CornerStyle.doubled
+        ? LineStyle.doubled
+        : LineStyle.single;
+
+    String x = lineStyle.x;
+    String y = lineStyle.y;
+
+    if (title?.style != CornerStyle.doubled && style == CornerStyle.doubled) {
+      x = LineStyle.doubled.x;
+      y = LineStyle.doubled.y;
+    }
+
+    if (title?.style == CornerStyle.doubled && style != CornerStyle.doubled) {
+      x = LineStyle.single.x;
+      y = LineStyle.single.y;
+    }
+
+    if (title != null) {
+      _renderTitle(title!, lineStyle, style, panelWidth, console);
     }
 
     if (title == null) {
       console.writeLine(
-        '${style.tl}${x * (width != null ? panelWidth - 2 : panelWidth + 2)}${style.tr}',
+        '${style.tl}${x * panelWidth}${style.tr}',
       );
     }
 
@@ -106,24 +128,60 @@ class ChubbyPanel {
       );
     }
 
+    int panelBottomWidth = panelWidth;
+
+    if (title != null) {
+      panelBottomWidth -= 2;
+    }
+
     console.writeLine(
-      '${style.bl}${x * ((panelWidth + 1) - (widthCompensation - 2) + (titleLength))}${style.br}',
+      '${style.bl}${x * panelBottomWidth}${style.br}',
     );
   }
 
-  int _calculatePanelWidth(
-    String text,
-    int titleLength,
-    int? width,
-    int consoleWidth,
-  ) {
-    final int textLength = text.length;
-    int panelWidth = textLength + titleLength;
+  int _calculateNumberOfLines(int? width, String text, String? title) {
+    final int consoleWidth = stdout.terminalColumns;
+    int calculatedWidth = consoleWidth;
 
-    if (width == null) panelWidth = textLength - titleLength;
-    if (width != null && width > textLength) panelWidth = width - titleLength;
-    if (width != null && width < textLength) panelWidth = width - titleLength;
-    if (panelWidth > consoleWidth) panelWidth = consoleWidth;
+    if (width != null && consoleWidth > width) calculatedWidth = width;
+
+    final int numLines = (text.length / (calculatedWidth - 4)).ceil();
+
+    return numLines;
+  }
+
+  int _calculatePanelWidth(int? width, String text, String? title) {
+    final int consoleWidth = stdout.terminalColumns;
+
+    int textLength = text.length + 4;
+
+    final int numLines = _calculateNumberOfLines(width, text, title);
+
+    if (numLines > 1) {
+      final int charactersPerLine = (text.length / numLines).round();
+      print("Characters per line: $charactersPerLine");
+      textLength = charactersPerLine + 4;
+    }
+
+    final int titleLength = title != null ? title.length + 4 : 0;
+
+    if (titleLength > textLength) textLength = titleLength;
+
+    print(
+      'Given Width: $width \nConsole Width: $consoleWidth \nActual Text Length: ${text.length} \nCalculated Text length: $textLength \nNumber of text lines: $numLines \n',
+    );
+
+    int panelWidth = consoleWidth;
+
+    if (width == null) {
+      print("No width specified. Using text width.");
+      if (textLength <= consoleWidth) panelWidth = textLength;
+    }
+
+    if (width != null && panelWidth > textLength) {
+      print("Width $width given; doing our best.");
+      return textLength;
+    }
 
     return panelWidth;
   }
